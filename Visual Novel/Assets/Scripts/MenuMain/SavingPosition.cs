632e144620c3a321d24cfd162991621ca6861392
Zhusik <zhaks04092005@gmail.com>
2024-04-03 19:25:47 +0600

    Menu
    xD

--- Visual Novel/Assets/Scripts/MenuMain/SavingPosition.cs
+++ Visual Novel/Assets/Scripts/MenuMain/SavingPosition.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class SavingPosition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject panelSettings;
 
-    // Update is called once per frame
-    void Update()
+    public void Settings()
     {
-        
+        if(panelSettings.activeSelf == false)
+        {
+            panelSettings.SetActive(true);
+        }
+        else if(panelSettings.activeSelf == true)
+        {
+            panelSettings.SetActive(false);
+        }
     }
 }
